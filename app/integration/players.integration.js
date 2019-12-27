ebb6508df18a59f3ac9c5909bd96d743dba12a33
Enrique Guerrero <enriqueguefer@gmail.com>
2019-12-27 01:00:03 +0100

    Métodos PUT y GET por Id para las integraciones de teams y players

--- app/integration/players.integration.js
+++ app/integration/players.integration.js
@@ -2,73 +2,72 @@
 const applicationConfig = require('../../config/application.config');
 const request = require("request-promise");
 
-module.exports.getPlayers = function getPlayers() {
+module.exports.getPlayers = async function getPlayers() {
   
-  let uri = applicationConfig.teamsPlayersAPI+'players'
-  console.log(Date() + " -GET "+ uri)
+    let uri = applicationConfig.teamsPlayersAPI+'players'
+    console.log(Date() + " -GET "+ uri)
 
-  let options = {
-    uri: uri,
-    method: 'GET',
-    json: true
-  }
+    let options = {
+      uri: uri,
+      method: 'GET',
+      json: true
+    }
 
-  return request(options)
-    .then((response) => {
-        console.log(Date() + "SUCCESS: -GET "+ uri)
-        console.log(response)
-        return response
-    }).catch((err) => {
-        console.log(Date() + "ERROR: -GET "+ uri)
-        console.error(err)
-        return Error(err)
-  })
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -GET " + uri);
+      console.log(response);
+      return response;
+    }catch (err) {
+      console.log(Date() + "ERROR: -GET " + uri);
+      console.error(err);
+      throw err;
+    }
 }
 
-module.exports.getPlayerById = function getPlayerById(playerId) {
+module.exports.getPlayerById = async function getPlayerById(playerId) {
   
-  let uri = applicationConfig.teamsPlayersAPI+'player?_id='+playerId
-  console.log(Date() + " -GET "+ uri)
+    let uri = applicationConfig.teamsPlayersAPI+'player?_id='+playerId
+    console.log(Date() + " -GET "+ uri)
 
-  let options = {
-    uri: uri,
-    method: 'GET',
-    json: true
-  }
+    let options = {
+      uri: uri,
+      method: 'GET',
+      json: true
+    }
 
-  return request(options)
-    .then((response) => {
-        console.log(Date() + "SUCCESS: -GET "+ uri)
-        console.log(response)
-        return response
-    }).catch((error) => {
-        console.log(Date() + "ERROR: -GET "+ uri)
-        console.error(error)
-        return Error(error)
-  })
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -GET " + uri);
+      console.log(response);
+      return response;
+    }catch (error) {
+      console.log(Date() + "ERROR: -GET " + uri);
+      console.error(error);
+      throw error;
+    }
 }
 
-module.exports.updatePlayer = function updatePlayer(updatePlayer) {
+module.exports.updatePlayer = async function updatePlayer(updatePlayer) {
   
-  let uri = applicationConfig.teamsPlayersAPI+'player'
-  console.log(Date() + " -PUT "+ uri)
-  console.log(Date() + " updatePlayer "+ updatePlayer)
+    let uri = applicationConfig.teamsPlayersAPI+'player'
+    console.log(Date() + " -PUT "+ uri)
 
-  let options = {
-    uri: uri,
-    json: true,
-    body: updatePlayer,
-    method: 'PUT'
+    let options = {
+      uri: uri,
+      json: true,
+      body: updatePlayer,
+      method: 'PUT'
+    }
+
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -PUT " + uri);
+      console.log(response);
+      return response;
+    }catch (error) {
+      console.log(Date() + "ERROR: -PUT " + uri);
+      console.error(error);
+      return Error(error);
   }
-
-  return request(options)
-    .then((response) => {
-        console.log(Date() + "SUCCESS: -PUT "+ uri)
-        console.log(response)
-        return response
-    }).catch((error) => {
-        console.log(Date() + "ERROR: -PUT "+ uri)
-        console.error(error)
-        return Error(error)
-  })
 }
--- app/integration/teams.integration.js
+++ app/integration/teams.integration.js
@@ -2,48 +2,95 @@
 const applicationConfig = require('../../config/application.config');
 const request = require("request-promise");
 
-module.exports.getTeams = function getTeams() {
+module.exports.getTeams = async function getTeams() {
   
-  let uri = applicationConfig.teamsPlayersAPI+'teams'
-  console.log(Date() + " -GET "+ uri)
+    let uri = applicationConfig.teamsPlayersAPI+'teams'
+    console.log(Date() + " -GET "+ uri)
 
-  let options = {
-    uri: uri,
-    method: 'GET',
-    json: true
-  }
+    let options = {
+      uri: uri,
+      method: 'GET',
+      json: true
+    }
 
-  return request(options)
-    .then((response) => {
-        console.log(Date() + "SUCCESS: -GET "+ uri)
-        console.log(response)
-        return response
-    }).catch((err) => {
-        console.log(Date() + "ERROR: -GET "+ uri)
-        console.error(err)
-        return Error(err)
-  })
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -GET " + uri);
+      console.log(response);
+      return response;
+    }catch (err) {
+      console.log(Date() + "ERROR: -GET " + uri);
+      console.error(err);
+      throw err;
+    }
 }
 
-module.exports.getTeamByName = function getTeamByName(teamName) {
+module.exports.getTeamById = async function getTeamById(teamId) {
   
-  let uri = applicationConfig.teamsPlayersAPI+'teams/'+teamName
-  console.log(Date() + " -GET "+ uri)
+    let uri = applicationConfig.teamsPlayersAPI+'teams/team/'+teamId
+    console.log(Date() + " -GET "+ uri)
 
-  let options = {
-    uri: uri,
-    method: 'GET',
-    json: true
-  }
+    let options = {
+      uri: uri,
+      method: 'GET',
+      json: true
+    }
 
-  return request(options)
-    .then((response) => {
-        console.log(Date() + "SUCCESS: -GET "+ uri)
-        console.log(response)
-        return response
-    }).catch((err) => {
-        console.log(Date() + "ERROR: -GET "+ uri)
-        console.error(err)
-        return Error(err)
-  })
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -GET " + uri);
+      console.log(response);
+      return response;
+    }catch (err) {
+      console.log(Date() + "ERROR: -GET " + uri);
+      console.error(err);
+      throw err;
+    }
+}
+
+module.exports.getTeamByName = async function getTeamByName(teamName) {
+  
+    let uri = applicationConfig.teamsPlayersAPI+'teams/'+teamName
+    console.log(Date() + " -GET "+ uri)
+
+    let options = {
+      uri: uri,
+      method: 'GET',
+      json: true
+    }
+
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -GET " + uri);
+      console.log(response);
+      return response;
+    }catch (err) {
+      console.log(Date() + "ERROR: -GET " + uri);
+      console.error(err);
+      throw err;
+    }
+}
+
+module.exports.updateTeam = async function updateTeam(updateTeam) {
+  
+    let uri = applicationConfig.teamsPlayersAPI+'teams/'+updateTeam.name
+    console.log(Date() + " -PUT "+ uri)
+
+    let options = {
+      uri: uri,
+      json: true,
+      body: updateTeam,
+      method: 'PUT'
+    }
+
+    try {
+      const response = await request(options);
+      console.log(Date() + "SUCCESS: -PUT " + uri);
+      console.log(response);
+      return response;
+    }catch (error) {
+      console.log(Date() + "ERROR: -PUT " + uri);
+      console.error(error);
+      throw error;
+    }
 }
